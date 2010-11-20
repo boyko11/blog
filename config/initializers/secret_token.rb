cf173cb472c9c891b7eb8080c3b37c7f4250ab48
Boyko Todorov <boyko11@gmail.com>
2010-11-20 11:05:55 -0500

    hoping to commit the entire project not just the readme file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '6a57a8696a0998054091df22b04bb2050991e0b150e6cd7dfa7089dbbac535387ebdea9b982df364778c9fa5f721c648d254ff5002c53a8d25e331961edd256c'
